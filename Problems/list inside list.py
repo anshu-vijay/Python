8b5586225754f9a3d00237150dda748b865b58ab
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-29 15:20:30 +0530

    lisi inside list
    maximum element from a list inside another list

---
+++ Problems/list inside list.py
@@ -0,0 +1,6 @@
+lst = [[8,59,6],[15,63,78],[5,9,3,7]]
+print(max(lst[1]))
+print("*******maximum from all the lists********")
+for i in range(len(lst)):
+    maximum = max(lst[i])
+    print(maximum)
