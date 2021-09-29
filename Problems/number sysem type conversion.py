ad158976f4c4a56d6ee0b5d97e099462dd4ebc77
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-29 18:21:47 +0530

    number system type conversion

---
+++ Problems/number sysem type conversion.py
@@ -0,0 +1,25 @@
+a=15
+print("decimal to binary")
+print(bin(a))
+
+print("binary to decimal")
+convert_a = bin(a)
+print(int(convert_a,2))
+
+
+b=55
+print("decimal to octal")
+print(oct(b))
+
+print("octal to decimal")
+convert_b = oct(b)
+print(int(convert_b,8))
+
+
+c=79
+print("decimal to hexa")
+print(hex(c))
+
+print("hexa to decimal")
+convert_c = hex(c)
+print(int(convert_c,16))
