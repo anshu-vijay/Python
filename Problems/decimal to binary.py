7ea24228adf301f266950e901cdff6d7e156a4dd
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-29 18:09:38 +0530

    decimal to binary

---
+++ Problems/decimal to binary.py
@@ -0,0 +1,17 @@
+import math
+
+a=10
+b=5
+sum=a+b
+print("Method1******decimal to binary******")
+print("decimal sum = ",sum)
+print(bin(sum))
+print("Method2*******binary to decimal*****")
+binary= 0
+base= 1
+while(sum>0):
+    rem = sum%2
+    binary = binary + rem*2
+    base = base*10
+    sum =sum/2
+print(sum)
