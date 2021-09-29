e881836f8ea29e1481340334544ed2e63a6de888
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-29 15:57:36 +0530

    prime with decimal values

---
+++ BasicPrograms/prime with decimal values.py
@@ -0,0 +1,18 @@
+import math
+
+start = int(input("Enter range starting number: "))
+end = int(input("Enter range ending number: "))
+count = 0
+if start>end:
+    print("start cannot be greater than end.")
+else:
+    for num in range(start,end+1):
+        if num>1:
+            for i in range(2,math.ceil(math.sqrt(num)+1)):
+                if num%i == 0:
+                    print(num,"{:.2f}".format(0))
+                    break
+            else:
+                print(num,"{:.2f}".format(num))
+                count = count + 1
+print("Total prime numbers in the range: ",count)
